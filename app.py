b58be6a6e2be239e82d3c55bd707241c8930f0d2
PrathameshDesai0409 <140812487+PrathameshDesai0409@users.noreply.github.com>
2024-05-29 21:52:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template,redirect,url_for,session
+import mysql.connector as mysql
+from datetime import timedelta
+
+app = (__name__)
+app.secret_key = 'abcdef'
+
+
+
+
+
+
+
+
+if __name__ = '__main__':
